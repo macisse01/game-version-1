2321a62a95088f74bb6c3eb7761d6dad9ec16308
macisse01 <158589888+macisse01@users.noreply.github.com>
2024-05-24 15:06:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+room  = {"Forest": "You're in a deep large forest. The burds are signing and the air smells fresh. despite this, something inside you is screaming at you to run away. ", 
+        
+         "Tavern" : "You enter a small broken down shack. However, upon entering, you you see a massive hall with a bar, a dining area, a commons area and a dacne ball.",
+
+         "Cave" : "You see a small light in the opening of a mountain. upon getting closer, the light mysteriously vanishes. Something's in there. ",
+
+         "bridge" : "You come across a rocky bridge connecting between the edge of two cliffs. On the othr side is a large tower. As you begin to turn away, The orb in you inventory begins to glow, possibly indicating that answer lie within the tower",
+
+
+         "Tower" : "You Enter a great tall dark tower. Inside you see great collection of books and chemical beakers "
+
+          
+         ""}
+
+
+Game_map = [
+["Tavern", "Merchant Town"],
+["Forest", "Bridge", "Tower", "Colloseaum"],
+["Cave", ]
+         ]
