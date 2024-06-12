ace6f25363644839aea6164e68fb38675ef204a4
macisse01 <158589888+macisse01@users.noreply.github.com>
2024-06-12 03:55:12 +0000

    added the enemy class

---
+++ enemy.py
@@ -0,0 +1,6 @@
+class Enemy:
+  def __init__(self, name, health, damage, description):
+      self.name = name
+      self.health = health
+      self.damage = damage
+      self.description = description
